e0d8b07ecc6ddb312e430ae3cdcccad99c259eb9
Sumit Kumar Chaudhary <sumit.chaudhary18@stu.srmuniversity.ac.in>
2023-04-23 17:29:31 +0530

    modify questions added new question

--- game.js
+++ game.js
@@ -11,49 +11,89 @@ let availablequestions=[];
 
 let questions = [
     {
-        question:"How do you initialize an array in C?",
-        choice1:"<int arr[3] = (1,2,3);>",
-        choice2:"<int arr(3) = {1,2,3};>",
-        choice3:"<int arr[3] = {1,2,3};>",
-        choice4:"<int arr(3) = (1,2,3);>",
-        answer: 3
+        question:"In the layer hierarchy as the data packet moves from the upper to the lower layers, headers are ___________",
+        choice1:"Added",
+        choice2:"Removed",
+        choice3:"Rearranged",
+        choice4:"Modified",
+        answer: 1
     },
     {
-        question:"How do you instantiate an array in Java?",
-        choice1:"<int arr[] = new int(3);>",
-        choice2:"< int arr[];>",
-        choice3:"<int arr[] = new int[3];>",
-        choice4:"<int arr() = new int(3);>",
-        answer: 3
+        question:"The structure or format of data is called ___________",
+        choice1:"Syntax",
+        choice2:" Semantics",
+        choice3:"Struct",
+        choice4:"Formatting",
+        answer: 1
     },
     {
-        question:"Which of the following is the correct way to declare a multidimensional array in Java?",
-        choice1:"<int[] arr;>",
-        choice2:"<int arr[[]];>",
-        choice3:"<int[][]arr;>",
-        choice4:"<int[[]] arr;>",
-        answer: 3
+        question:"Communication between a computer and a keyboard involves ______________ transmission.",
+        choice1:"Automatic",
+        choice2:"Half-duplex",
+        choice3:"Full-duplex",
+        choice4:"Simplex",
+        answer: 4
     },
     {
-        question:"When does the Array Index Out Of Bounds Exception occur?",
-        choice1:"<Compile-time>",
-        choice2:"<Run-time>",
-        choice3:"<Not an error>",
-        choice4:"<Not an exception at all>",
+        question:"A _______ is the physical path over which a message travels.?",
+        choice1:"Path",
+        choice2:"Medium",
+        choice3:"Protocol",
+        choice4:"Route",
         answer: 2
     },
     {
-        question:"Which of the following concepts make extensive use of arrays?",
-        choice1:"<Binary trees>",
-        choice2:"<Scheduling of processes>",
-        choice3:"<Caching>",
-        choice4:"<Spatial locality>",
+        question:"Which of this is not a network edge device?",
+        choice1:"PC",
+        choice2:"Smartphones",
+        choice3:"Servers",
+        choice4:"Switches",
         answer: 4
+    },
+    {
+        question:"A _________ set of rules that governs data communication.",
+        choice1:"Protocols",
+        choice2:"Standars",
+        choice3:"RFCs",
+        choice4:"Servers",
+        answer: 1
+    },
+    {
+        question:"Which of the following computer networks is built on the top of another network?",
+        choice1:"prior network",
+        choice2:"chief network",
+        choice3:"prime network",
+        choice4:"overlay network",
+        answer: 4
+    },
+    {
+        question:"Communication channel is shared by all the machines on the network in ________",
+        choice1:"broadcast network",
+        choice2:"unicast network",
+        choice3:"multicast network",
+        choice4:"anycast network",
+        answer: 1
+    },
+    {
+        question:"Bluetooth is an example of __________",
+        choice1:"personal area network",
+        choice2:"local area network",
+        choice3:"virtual private network",
+        choice4:"wide area network",
+        answer: 1
+    },
+    {
+        question:"A __________ is a device that forwards packets between networks by processing the routing information included in the packet.",
+        choice1:"bridge",
+        choice2:"firewall",
+        choice3:"router",
+        choice4:"hub",
+        answer: 3
     }
 ];
 
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 5;
+const CORRECT_BONUS = 2;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questioncounter = 0;
